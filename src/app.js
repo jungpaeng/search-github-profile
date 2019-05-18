9631cb833a17911131e95788030a36ad01db8f5c
jungpaeng <meis1541@naver.com>
2019-05-18 19:49:39 +0900

    :sparkles: Add Save Image to Canvas

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "0d0d0d";
 const CANVAS_SIZE = 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -45,6 +48,10 @@ const stopPainting = () => {
   painting = false;
 };
 
+const hangleCM = event => {
+  event.preventDefault();
+};
+
 const handleColorClick = event => {
   const {
     target: {
@@ -74,11 +81,21 @@ const handleModeClick = () => {
   }
 };
 
+const handleSaveClick = () => {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+
+  link.href = image;
+  link.download = new Date().getTime().toString();
+  link.click();
+};
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("contextmenu", hangleCM);
 }
 
 Array.from(colors).forEach(color =>
@@ -91,4 +108,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
