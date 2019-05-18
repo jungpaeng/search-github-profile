fe03936daadf92b83f64187dd8b9b3de0e2f9a89
jungpaeng <meis1541@naver.com>
2019-05-18 19:07:00 +0900

    :sparkles: Add Line Draw to Canvas

--- src/app.js
+++ src/app.js
@@ -1,27 +1,37 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#0d0d0d";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+const onMouseMove = event => {
+  const { offsetX, offsetY } = event;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(offsetX, offsetY);
+  } else {
+    ctx.stroke();
+    ctx.lineTo(offsetX, offsetY);
+  }
+};
+
+const startPainting = () => {
+  painting = true;
+};
 
 const stopPainting = () => {
   painting = false;
 };
 
-const onMouseMove = event => {
-  const { offsetX, offsetY } = event;
-  console.log(offsetX, offsetY);
-};
-
-const onMouseDown = () => {
-  painting = true;
-};
-
-const onMouseUp = () => {
-  stopPainting();
-};
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
